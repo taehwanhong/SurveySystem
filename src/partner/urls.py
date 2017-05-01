ea838bc5c3316aca0377527c8fc3850d463f253a
davidhong <taehwan.hong@gmail.com>
2017-05-01 16:00:35 +0900

    (MAJOR_CREATE) Sign up page create

--- src/partner/urls.py
+++ src/partner/urls.py
@@ -13,11 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
-from django.contrib import admin
 
+
+from django.conf.urls import url, include
+from .views import signup, index
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),  
-    url(r'^partner/', include('partner.urls')),
+    url(r'^signup/$', signup, name="signup"),
+    url(r'^$', index, name="index"),
 ]
+# python reg exp
--- src/partner/views.py
+++ src/partner/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+
 
 # Create your views here.
+def index(request):
+    ctx={}
+    return render(request, "index.html", ctx)
+
+def signup(request):
+    if request.method =="GET":
+        pass
+    elif request.method =="POST":
+        username = request.POST.get("username")
+        email= request.POST.get("email")
+        password= request.POST.get("password")
+        user = User.objects.create_user(username, email, password)
+
+        # print(username, email, password)
+    ctx={}
+    return render(request, "signup.html", ctx)
+
